055fdce0f346198ea69ba1ca28213889b74cb0ec
dangkhoa1605 <dangkhoa1605@gmail.com>
2026-05-24 10:11:30 +0700

    chore: remove unused directories backup_temp, migrated_prompt_history, and .tmp_pw

--- backup_temp/global.d.ts
+++
@@ -1,8 +0,0 @@
-
-// Global type definitions for external libraries loaded via CDN
-declare const google: any;
-declare const lucide: {
-    createIcons: (options?: { root?: HTMLElement | null }) => void;
-};
-declare const Sortable: any;
-declare const html2canvas: any;
