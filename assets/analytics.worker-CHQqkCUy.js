f4298655ebbe901d74b35b28796a82fcb52a0daa
ltsdata1605-glitch <ltsdata1605-glitch@users.noreply.github.com>
2026-07-05 18:19:42 +0700

    Updates

---
+++ assets/analytics.worker-CHQqkCUy.js
@@ -0,0 +1,1 @@
+function r(e){return new Worker("/assets/analytics.worker-DiK6t-3n.js",{name:e==null?void 0:e.name})}export{r as default};
--- assets/analytics.worker-Dt6pVj_K.js
+++
@@ -1,1 +0,0 @@
-function e(r){return new Worker("/assets/analytics.worker-CprRl9fu.js",{name:r==null?void 0:r.name})}export{e as default};
